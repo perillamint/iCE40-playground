e67d62b91c731048b1b3df2a36defc0a20564ab1
perillamint <perillamint@quendi.moe>
2018-09-24 00:23:29 +0900

    LED counter example

--- led_counter/led_counter.v
+++ led_counter/led_counter.v
@@ -1,5 +1,6 @@
 module top (
 	input  clk,
+  input  rst,
 	output LED0,
 	output LED1,
 	output LED2,
@@ -7,25 +8,20 @@ module top (
 	output LED4,
 	output LED5,
 	output LED6,
-	output LED7,
+	output LED7
 );
 
    localparam CLKDIV = 21;
 
    reg [CLKDIV:0] clkdiv;
-   reg [7:0]     counter;
-   
+   wire [7:0]     led;
+
    always@(posedge clk)
      begin
         clkdiv <= clkdiv + 1;
+     end
 
-        if (clkdiv[CLKDIV])
-          begin
-             clkdiv <= 0;
-	         counter <= counter + 1;
-          end
+   counter counter1 (clkdiv[CLKDIV], rst, led);
 
-     end // always@ (posedge clk)
-
-   assign {LED0, LED1, LED2, LED3, LED4, LED5, LED6, LED7} = counter;
+   assign {LED0, LED1, LED2, LED3, LED4, LED5, LED6, LED7} = led;
 endmodule
